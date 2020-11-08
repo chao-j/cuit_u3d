c475660982db26a67e127489409c1fa5773d9e3c
Zhaocj <2516304799@qq.com>
2020-11-09 00:08:16 +0800

    feat:引入UII及相关控制

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public List<string> contents;
+    public UIPanel uiPanel;
+    [SerializeField]
+    private int curLine;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            Init();
+            LoadText(contents[curLine]);
+            ShowUI();
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            NextLine();
+            if (curLine >= contents.Count)
+            {
+                Init();
+            }
+            LoadText(contents[curLine]);
+        }
+    }
+
+    private void Init()
+    {
+        HideUI();
+        curLine = 0;
+        uiPanel.SetDigText("");
+    }
+
+    private void ShowUI()
+    {
+        uiPanel.ShowCharaA(true);
+        uiPanel.ShowCharaB(true);
+        uiPanel.ShowDigBg(true);
+        uiPanel.ShowDigText(true);
+    }
+
+    private void HideUI()
+    {
+        uiPanel.ShowCharaA(false);
+        uiPanel.ShowCharaB(false);
+        uiPanel.ShowDigBg(false);
+        uiPanel.ShowDigText(false);
+    }
+
+    private void NextLine()
+    {
+        curLine++;
+    }
+
+    private void LoadText(string text)
+    {
+        uiPanel.SetDigText(text);
+    }
+}
---
+++ Assets/Scripts/UIPanel.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPanel : MonoBehaviour
+{
+    public RawImage charaAImg;
+    public RawImage charaBImg;
+    public Image digBgImg;
+    public Text digText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    // 控制主角色显示
+    public void ShowCharaA(bool isShow)
+    {
+        charaAImg.enabled = isShow;
+    }
+    // 控制副角色显示
+    public void ShowCharaB(bool isShow)
+    {
+        charaBImg.enabled = isShow;
+    }
+    // 控制对话框背景显示
+    public void ShowDigBg(bool isShow)
+    {
+        digBgImg.enabled = isShow;
+    }
+
+    // 控制对话文字显示
+    public void ShowDigText(bool isShow)
+    {
+        digText.enabled = isShow;
+    }
+
+    // 设置对话文字
+    public void SetDigText(string text)
+    {
+        digText.text = text;
+    }
+
+}
